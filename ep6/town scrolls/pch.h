b3695b8a844d516ac289aa9a7e8be2969974ba72
426f776965 <67214185+426f776965@users.noreply.github.com>
2020-09-19 01:02:08 -0400

    Update pch.h

--- ep6/town scrolls/pch.h
+++ ep6/town scrolls/pch.h
@@ -4,18 +4,16 @@
 #include <windows.h>
 //declarations
 void TownFunc();
-bool Hook(void* addr, void* func, int len);
+bool Hook(void* addr, void* func, int len = 5);
 //expressions
 int packet_retn = 0x4784DB;
 int case_0x050A = 0x478F8D;
 int effect_retn = 0x473170;
 int effect_jump = 0x47469F;
 int effect_fail = 0x47468A;
-int cancel_retn = 0x49AE66;
-int cancel_fail = 0x49AEBA;
+int type_retn = 0x49DDC8;
+int type_jump = 0x49DEB5;
 //ps_game functions
 int ItemUseNSend = 0x4728E0;
 int SConnectionSend = 0x4ED0E0;
 int PSendViewCombat = 0x4D56D0;
-//packet buffer
-unsigned char tele_packet[8];
